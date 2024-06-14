e6209de0ce9b3c576e74b9f9a173c2882ea1372b
animesh <sethanimesh@hotmail.com>
2024-06-14 14:45:55 +0530

    airplane mode

---
+++ BroadcastApp/app/src/main/java/com/example/broadcastapp/AirplaneModeReceiver.java
@@ -0,0 +1,24 @@
+package com.example.broadcastapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AirplaneModeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // This method is called when the Broadcast Receiver
+        // receives a broadcast
+
+        if (intent.getAction() != null &&
+                intent.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)){
+            boolean isAirplaneModeOn = intent.getBooleanExtra("state", false);
+
+            String msg = isAirplaneModeOn ? "Airplane Mode is ON" : "Airplane Mode is OFF";
+
+            Toast.makeText(context, ""+msg, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
---
+++ BroadcastApp/app/src/main/java/com/example/broadcastapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.broadcastapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Register Dynamically the BroadCast Receiver
+        IntentFilter intentFilter = new IntentFilter("android.intent.action.AIRPLANE_MODE");
+        AirplaneModeReceiver br = new AirplaneModeReceiver();
+        registerReceiver(br, intentFilter);
+
+    }
+}
